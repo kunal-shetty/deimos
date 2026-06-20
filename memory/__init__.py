62e9aeed7842e0c7c04d2e4379b09b4ca2a3a19a
kunal-shetty <kunalshetty567@gmail.com>
2026-06-20 22:18:26 +0530

    feat: export ProjectMemory and score_message from memory package

--- memory/__init__.py
+++ memory/__init__.py
@@ -2,5 +2,8 @@ from .manager import MemoryManager
 from .conversation import ConversationStore
 from .episodic import EpisodicMemory
 from .semantic import SemanticMemory
+from .project import ProjectMemory
+from .active import score_message
 
-__all__ = ["MemoryManager", "ConversationStore", "EpisodicMemory", "SemanticMemory"]
+__all__ = ["MemoryManager", "ConversationStore", "EpisodicMemory",
+           "SemanticMemory", "ProjectMemory", "score_message"]
