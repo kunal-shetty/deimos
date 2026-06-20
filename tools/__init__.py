9bb1ec6aff4b207a3c6892b63da40de74e093e29
kunal-shetty <kunalshetty567@gmail.com>
2026-06-20 22:18:28 +0530

    feat: export new tools (edit_file, list_directory, search_codebase, skills, create_docx)

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,6 +2,17 @@ from .registry import ToolRegistry
 from .base import BaseTool
 from .read_file import ReadFileTool
 from .write_file import WriteFileTool
+from .edit_file import EditFileTool
 from .run_command import RunCommandTool
+from .list_directory import ListDirectoryTool
+from .search_codebase import SearchCodebaseTool
+from .list_skills import ListSkillsTool
+from .read_skill import ReadSkillTool
+from .create_docx import CreateDocxTool
 
-__all__ = ["ToolRegistry", "BaseTool", "ReadFileTool", "WriteFileTool", "RunCommandTool"]
+__all__ = [
+    "ToolRegistry", "BaseTool",
+    "ReadFileTool", "WriteFileTool", "EditFileTool",
+    "RunCommandTool", "ListDirectoryTool", "SearchCodebaseTool",
+    "ListSkillsTool", "ReadSkillTool", "CreateDocxTool",
+]
